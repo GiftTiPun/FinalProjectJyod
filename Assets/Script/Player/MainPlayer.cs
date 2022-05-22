89729183987f0d0c1caf0b1711f44397857d7df5
zonyxe <thichakorn.ticha@mail.kmutt.ac.th>
2022-05-22 21:13:34 +0700

    Movement-NetCode
    สร้างเซิฟเดินกันแบบIsoแบบหลายคนได้แล้ว

---
+++ Assets/Script/Player/MainPlayer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
+
+public class MainPlayer : NetworkBehaviour
+{
+    public NetworkVariable<Vector2> Position = new NetworkVariable<Vector2>();
+
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(IsClient && IsOwner)
+        {
+            moveH = Input.GetAxis("Horizontal") * moveSpeed;
+            moveV = Input.GetAxis("Vertical") * moveSpeed;
+            rb.velocity = new Vector2(moveH, moveV);
+        }
+        
+    }
+    public override void OnNetworkSpawn()
+    {
+        Move();
+    }
+    public void Move()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            var randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+        }
+        else
+        {
+            SubmitPositionRequestServerRpc();
+        }
+
+    }
+
+    [ServerRpc]
+    void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
+    {
+        Position.Value = GetRandomPositionOnPlane();
+    }
+
+    static Vector3 GetRandomPositionOnPlane()
+    {
+        return new Vector2(Random.Range(-3f, 3f),Random.Range(-3f, 3f));
+    }
+
+    
+
+}
