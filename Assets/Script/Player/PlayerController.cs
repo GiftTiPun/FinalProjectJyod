0dc4ea475e3d474cfbcea0caf23513339870ddd3
zonyxe <thichakorn.ticha@mail.kmutt.ac.th>
2022-05-22 16:05:39 +0700

    playerMovement_single
    ทำเคลื่อนที่แต่ยังไม่ได้ใส่แบบnetcode

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        moveV = Input.GetAxis("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(moveH, moveV);
     }
+
+   
+
 }
